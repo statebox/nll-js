ad42e9b9c6091d565d728c29fc537747873c8a68
wires <jelle@defekt.nl>
2017-04-30 13:22:55 +0200

    test, ci, integers instead of naturals

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const jsc = require("jsverify");
+const varints = require('../varint-list.js')
+const R = require('ramda')
+
+const isIdentity = jsc
+	.forall('nearray integer', function(nats) {
+		const e = varints.encode(nats)
+		const d = varints.decode(e)
+		return R.equals(d, nats)
+	})
+
+
+jsc.assert(isIdentity);
--- varint-list.js
+++ varint-list.js
@@ -1,8 +1,21 @@
-var varint = require('varint')
+var varint = require('signed-varint')
 
 const concata = (a0,a1) => a0.concat(a1)
 
+const isEmptyList = l => !(l && l.length > 0)
+
 exports.encode = function (listOfNumbers) {
+	
+	if(isEmptyList(listOfNumbers)) {
+		throw new Error('must pass non empty list of numbers')
+	}
+
+	listOfNumbers.forEach(n => {
+		if (typeof n !== 'number') {
+			throw new Error('must pass list of numbers, `' + n + '` is not a number')
+		}
+	})
+
 	const bytes = listOfNumbers
 		.map(n => new varint.encode(n))
 		.reduce(concata)
@@ -13,12 +26,14 @@ exports.decode = function (buffer) {
 	var numbers = []
 	var skip = 0
 	try {
-		while(true) {
+		while(skip < buffer.length) {
 			numbers.push(varint.decode(buffer, skip))
 			skip += varint.decode.bytes
 		}
+		
+		return numbers;
 	}
 	catch(e) {
-		return numbers
+		throw new Error('failed to decode\n' + e)
 	}
 }
